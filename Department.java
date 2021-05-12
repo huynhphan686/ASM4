a25040a09b16232a39882cbaaa981505ee8d3dca
huynhphan686 <63278157+huynhphan686@users.noreply.github.com>
2021-05-12 20:28:24 +0700

    Update Department.java

--- Department.java
+++ Department.java
@@ -15,4 +15,60 @@ public class Department {
     
     List<Staff> listStaff = new ArrayList<>();
     
+    
+    /**
+     * @return the maBp
+     */
+    public String getMaBp() {
+        return maBp;
+    }
+
+    /**
+     * @param maBp the maBp to set
+     */
+    public void setMaBp(String maBp) {
+        this.maBp = maBp;
+    }
+
+    /**
+     * @return the tenBp
+     */
+    public String getTenBp() {
+        return tenBp;
+    }
+
+    /**
+     * @param tenBp the tenBp to set
+     */
+    public void setTenBp(String tenBp) {
+        this.tenBp = tenBp;
+    }
+
+    /**
+     * @return the soLuongNv
+     */
+    public int getSoLuongNv() {
+        return soLuongNv;
+    }
+
+    /**
+     * @param soLuongNv the soLuongNv to set
+     */
+    public void setSoLuongNv(int soLuongNv) {
+        this.soLuongNv = soLuongNv;
+    }
+    
+    /**
+     * @return the dsNV
+     */
+    public List<Staff> getListStaff() {
+        return listStaff;
+    }
+
+    /**
+     * @param dsNV the dsNV to set
+     */
+    public void setListStaff(List<Staff> listStaff) {
+        this.listStaff = listStaff;
+    }
 }
